5c1244cbc6258d2e74c46a534cc5a609fd9e6f2b
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-05-22 16:51:01 +0300

    Сделана задача 3

--- задача_3_функции.py
+++ задача_3_функции.py
@@ -1,7 +1,8 @@
 documents = [
 	{"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
 	{"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
-	{"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
+	{"type": "insurance", "number": "10006", "name": "Аристарх Павлов"},
+	{"type": "insurance", "name": "123123"}
 ]
 
 directories = {
@@ -17,14 +18,41 @@ def main():
 		if user_input == 'p':
 			person(documents)
 		elif user_input == 's':
-			shelf()
+			shelf(directories)
 		elif user_input == 'l':
-			documents_list(documents)
+			try:
+				documents_list(documents)
+			except KeyError as e:
+				print('В одном из документов отсутствует ключ', e)
+			# try\except для команды 'l' выводит недостающий ключ на экран <<<-----------------------
+		elif user_input == 'ls':
+			list_shelfs(directories)
+		elif user_input == 'd':
+			delete(documents, directories)
 		elif user_input == 'a':
 			add_document(documents, directories)
+		elif user_input == 'as':
+			add_shelf(directories)
+		elif user_input == 'm':
+			move(directories)
 		elif user_input == 'q':
-			goodbye()
 			break
+
+
+def documents_list(documents):
+	'''
+	l – list – команда, которая выведет список всех документов
+	'''
+	for client in documents:
+		print(f'{client["type"]} "{client["number"]}" "{client["name"]}"')
+
+
+def list_shelfs(directories):
+	'''
+	ls (list shelfs) - команда, которая выводит на экран список полок и документов, которые на них находятся
+	'''
+	for shelf, docs in directories.items():
+		print(f'Полка №{shelf}: {docs}')
 
 
 def person(data_base):
@@ -32,37 +60,33 @@ def person(data_base):
 	p – people – команда, которая спросит номер документа
 	 и выведет имя человека, которому он принадлежит
 	'''
-	user_input = input('Номер документа: ')
-	for client in data_base:
-		if client['number'] == user_input:
-			print(client['name'], '\n')
+	directories_docs = []  # как будет время сделаю эту проверку через отдельную функцию, сорри отстал от программы просто(
 
+	for x in directories.values():
+		directories_docs = directories_docs + x
 
-def shelf():
-	'''
-	s – shelf – команда, которая спросит номер документа
-	и выведет номер полки, на которой он находится;
-	'''
 	user_input_number = input('Номер документа: ')
 
-	for shelf, doc in directories.items():
-		while user_input_number not in doc:
-			user_input_number = input('Документа с таким номером не существует!\nНомер документа: ')
-		if user_input_number in doc:
-			print(f'Документа находится на полке №{shelf} \n')
-		break
+	while user_input_number not in directories_docs:
+		user_input_number = input('Документа с таким номером не существует \nНомер документа: ')
+
+	for client in data_base:
+		if client['number'] == user_input_number:
+			print(client['name'])
 
 
-def documents_list(documents):
+def shelf(directories):
 	'''
-	l– list – команда, которая выведет список всех документов
-	 в формате passport "2207 876234" "Василий Гупкин";
+	s – shelf – команда, которая спросит номер документа и выведет номер полки, на которой он находится
 	'''
-	print()
-	for client in documents:
-		print(
-			f'{client["type"]} "{client["number"]}" "{client["name"]}"')
-	print()
+	user_input_number = input('Номер документа: ')
+	for key, value in directories.items():
+		# print(value)
+		if user_input_number in value:
+			print(f'Документа находится на полке №{key}')
+			break
+	else:
+		print('Такого документа нет')
 
 
 def add_document(documents, directories):
@@ -70,9 +94,7 @@ def add_document(documents, directories):
 	a – add – команда, которая добавит новый документ
 	в каталог и в перечень полок, спросив его номер,
 	тип, имя владельца и номер полки, на котором
-	он будет храниться. Корректно обработайте ситуацию,
-	когда пользователь будет пытаться добавить документ
-	на несуществующую полку.
+	он будет храниться
 	'''
 	user_input_type = input('Тип документа: ')
 	user_input_number = input('Номер документа: ')
@@ -93,8 +115,60 @@ def add_document(documents, directories):
 	print('Документ добавлен!')
 
 
-def goodbye():
-	print('До свидания!')
+def delete(documents, directories):
+	'''
+	d – delete – команда, которая спросит номер документа и удалит его из каталога и из перечня полок
+	 '''
+
+	directories_docs = []
+
+	for x in directories.values():
+		directories_docs = directories_docs + x
+
+	user_input_number = input('Номер документа: ')
+
+	while user_input_number not in directories_docs:
+		user_input_number = input('Документа с таким номером не существует \nНомер документа: ')
+
+	for client in documents:
+		if user_input_number == client['number']:
+			documents.remove(client)
+
+
+def add_shelf(directories):
+	user_input_shelf = input('Номер новой полки: ')
+
+	while user_input_shelf in directories.keys():
+		user_input_shelf = input('Полка с таким номером уже существует! \nВведите другой номер: ')
+
+	directories.setdefault(user_input_shelf, [])
+
+
+def move(directories):
+	'''
+	d – delete – команда, которая спросит номер документа и удалит его из каталога и из перечня полок
+	 '''
+
+	directories_docs = []
+
+	for x in directories.values():
+		directories_docs = directories_docs + x
+
+	user_input_number = input('Номер документа: ')
+
+	while user_input_number not in directories_docs:
+		user_input_number = input('Документа с таким номером не существует \nНомер документа: ')
+
+	user_input_shelf = input('Номер целевой полки: ')
+
+	while user_input_shelf not in directories.keys():
+		user_input_shelf = input('Полки с таким номером не существует! \nВведите номер полки: ')
+
+	for key, value in directories.items():
+		if user_input_number in value:
+			value.remove(user_input_number)
+
+	directories[user_input_shelf].append(user_input_number)
 
 
 main()
