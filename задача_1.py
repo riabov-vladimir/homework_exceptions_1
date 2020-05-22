d3e656307c3b691b0201880e63625b7d119feece
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-05-22 16:57:25 +0300

    Удалён файл Задача_2

--- задача_1.py
+++ задача_1.py
@@ -1,4 +1,14 @@
 def polish_notation():
+	user_input = input('Введите выражение в формате [x y z], где x - оператор, а y, z - операнды \n')
+	operator = user_input[0]
+
+	try:
+		assert operator in ['+', '-', '/', '*'], 'Недопустимая операция'
+	except Exception as e:
+		print('Assertion error:', e)
+
+	operand_1 = int(user_input[2])
+	operand_2 = int(user_input[4])
 	if operator == '+':
 		return operand_1 + operand_2
 	elif operator == '-':
@@ -8,24 +18,50 @@ def polish_notation():
 	elif operator == '/':
 		return operand_1 / operand_2
 
-
-user_input = input('Введите выражение в формате [x y z], где x - оператор, а y, z - операнды \n')
-
-operator = user_input[0]
-
-try:
-	operand_1 = int(user_input[2])
-	operand_2 = int(user_input[4])
-except (IndexError, Exception) as e:
-	print('Error:', e)
-
-try:
-	assert operator in ['+','-','/','*'], 'Недопустимая операция'
-except Exception as e:
-	print('Assertion error:', e)
-
-
 try:
 	print(polish_notation())
-except ZeroDivisionError as e:
+except Exception as e:
 	print('Error:', e)
+
+
+
+
+#
+# try:
+# 	assert operator in ['+','-','/','*'], 'Недопустимая операция'
+# except Exception as e:
+# 	print('Assertion error:', e)
+
+
+
+# def polish_notation():
+# 	if operator == '+':
+# 		return operand_1 + operand_2
+# 	elif operator == '-':
+# 		return operand_1 - operand_2
+# 	elif operator == '*':
+# 		return operand_1 * operand_2
+# 	elif operator == '/':
+# 		return operand_1 / operand_2
+#
+#
+# user_input = input('Введите выражение в формате [x y z], где x - оператор, а y, z - операнды \n')
+#
+# operator = user_input[0]
+#
+# try:
+# 	operand_1 = int(user_input[2])
+# 	operand_2 = int(user_input[4])
+# except (IndexError, Exception) as e:
+# 	print('Error:', e)
+#
+# try:
+# 	assert operator in ['+','-','/','*'], 'Недопустимая операция'
+# except Exception as e:
+# 	print('Assertion error:', e)
+#
+#
+# try:
+# 	print(polish_notation())
+# except ZeroDivisionError as e:
+# 	print('Error:', e)
